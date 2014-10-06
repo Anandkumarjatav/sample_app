4d837c61b4b7ae3bdbcd2e110e9d19a79f515691
Anand Kumar Jatav <anandkumarjatav@gmail.com>
2014-10-06 06:10:11 +0530

    Restructure example in user_pages_spec

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -30,18 +30,18 @@ describe "User pages" do
 
 		describe "delete links" do
 			it { should_not have_link('delete') }
-		end
 
-		describe "as an admin user" do
-			let(:admin) { FactoryGirl.create(:admin) }
-			before do
-				sign_in admin
-				visit users_path
-			end
-			it { should have_link('delete', href: user_path(User.first)) }
+			describe "as an admin user" do
+				let(:admin) { FactoryGirl.create(:admin) }
+				before do
+					sign_in admin
+					visit users_path
+				end
+				it { should have_link('delete', href: user_path(User.first)) }
 
-			it "should be able to delete user" do
-				expect { click_link 'delete'}.to change(User, :count).by(-1)
+				it "should be able to delete user" do
+					expect { click_link 'delete'}.to change(User, :count).by(-1)
+				end
 			end
 		end
 	end
